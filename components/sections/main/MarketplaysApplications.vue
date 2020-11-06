1e6d483cb0a5c6c01ca2061bbb221b41d4169e4c
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-11-06 21:59:05 +0800

    add link on the marketplays app section, hover line

--- components/sections/main/MarketplaysApplications.vue
+++ components/sections/main/MarketplaysApplications.vue
@@ -15,37 +15,39 @@
                         v-for="item in items"
                     >
                         <v-hover v-slot:default="{ hover }">
-                            <v-card 
-                                outlined
-                                class="py-2"
-                                :style="hover ? 'background: linear-gradient(225deg, #E25405 0%, #CA0316 100%), linear-gradient(0deg, #FFFFFF, #FFFFFF)' : ''"
-                            >
-                                <v-card-title >
-                                    <h5 :class="{ 'font-weight-light d-block': true, 'white--text': hover }">
-                                        {{ item.title }}
-                                    </h5>
-                                </v-card-title>
-                                <v-divider class="mx-4 mb-2"></v-divider>
-                                <v-card-text>
-                                    <p 
-                                        :class="{ 'white--text': hover }" 
-                                        style="max-width: 235px" 
-                                        v-html="item.content"
-                                    ></p>
-                                </v-card-text>
-                                <v-card-actions>
-                                    <NuxtLink :to="item.link || '#'" style="text-decoration: none">
-                                        <v-btn 
-                                            :class="{ 'font-weight-bold': true, 'white--text': hover }"
-                                            :color="hover ? 'white' : 'primary'"
-                                            text
-                                            v-html="item.btnLabel"
-                                            light
-                                        >
-                                        </v-btn>
-                                    </NuxtLink>
-                                </v-card-actions>
-                            </v-card>
+                            <NuxtLink :to="item.link || ''">
+                                <v-card 
+                                    outlined
+                                    class="py-2"
+                                    :style="hover ? 'background: linear-gradient(225deg, #E25405 0%, #CA0316 100%), linear-gradient(0deg, #FFFFFF, #FFFFFF)' : ''"
+                                >
+                                    <v-card-title >
+                                        <h5 :class="{ 'font-weight-light d-block': true, 'white--text': hover }">
+                                            {{ item.title }}
+                                        </h5>
+                                    </v-card-title>
+                                    <v-divider class="mx-4 mb-2" :color="hover ? 'white' : ''"></v-divider>
+                                    <v-card-text>
+                                        <p 
+                                            :class="{ 'white--text': hover }" 
+                                            style="max-width: 235px" 
+                                            v-html="item.content"
+                                        ></p>
+                                    </v-card-text>
+                                    <v-card-actions>
+                                        <NuxtLink :to="item.link || '#'" style="text-decoration: none">
+                                            <v-btn 
+                                                :class="{ 'font-weight-bold': true, 'white--text': hover }"
+                                                :color="hover ? 'white' : 'primary'"
+                                                text
+                                                v-html="item.btnLabel"
+                                                light
+                                            >
+                                            </v-btn>
+                                        </NuxtLink>
+                                    </v-card-actions>
+                                </v-card>
+                            </NuxtLink>
                         </v-hover>
                     </v-col>
                 </v-row>
