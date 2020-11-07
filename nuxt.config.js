e93713de99923a8efbd5be86a34ebfdde8701b11
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-11-07 20:49:03 +0800

    add single service page

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,7 @@ export default {
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
+    treeShake: true,
     customVariables: ['~/assets/variables.scss'],
     theme: {
       light: true,
