aa9eb787377fd1eb1a1f493113fb3c7572f2f369
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-12-08 13:23:26 +0800

    Renaming header nav

--- layouts/ecommerce.vue
+++ layouts/ecommerce.vue
@@ -1,29 +1,29 @@
 <template>
-	<v-app :class="bodyClassName">
-		<header-layout />
-        <header-nav />
-		<v-main>
-			<router-view></router-view>
-		</v-main>
-		<footer-layout />
-	</v-app>
+  <v-app :class="bodyClassName">
+    <header-layout />
+    <header-nav-ecommerce />
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <footer-layout />
+  </v-app>
 </template>
 
 <script>
 import HeaderLayout from '~/components/Header'
-import HeaderNav from '~/components/HeaderNav'
+import HeaderNavEcommerce from '~/components/HeaderNavEcommerce'
 import FooterLayout from '~/components/Footer'
 export default {
-	name: 'default-template',
-	components: {
-		HeaderLayout,
-        FooterLayout,
-        HeaderNav
-	},
-	computed: {
-		bodyClassName() {
-			return this.$route.name ? this.$route.name.toLowerCase() : ""
-		}
-	},
-};
+  name: 'EcommerceTemplate',
+  components: {
+    HeaderLayout,
+    FooterLayout,
+    HeaderNavEcommerce,
+  },
+  computed: {
+    bodyClassName() {
+      return this.$route.name ? this.$route.name.toLowerCase() : ''
+    },
+  },
+}
 </script>
