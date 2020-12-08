d812f1e99815ac671b93dd7821d59cb2be5290e3
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-12-08 15:24:23 +0800

    update global mixin spacing

--- plugins/GlobalMixins.js
+++ plugins/GlobalMixins.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 
-const GlobalMixins = {
-    data: () => ({
-        containerSize: '1280px',
-        storeUrl: '/store'
-    })
-}
-
-Vue.mixin(GlobalMixins)
+Vue.mixin({
+  data: () => ({
+    containerSize: '1280px',
+    storeUrl: '/store',
+  }),
+})
